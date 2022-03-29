aff79ef0e456d732991e8372ed3b45d52b528aa2
calgrimes <calgrimes29@gmail.com>
2022-03-29 19:02:06 +0100

    Hyper fix, clear text from another page

--- index.php
+++ index.php
@@ -6,10 +6,5 @@ $file = 'readings.txt';
 
 file_put_contents($file, $entityBody . PHP_EOL, FILE_APPEND | LOCK_EX);
 
-if(isset($_GET['clear'])) {
-    file_put_contents($file, "");
-    $_GET['clear'] = NULL
-}
-
 
 ?>
