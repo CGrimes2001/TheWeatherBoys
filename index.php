2297fb163471fd69471a9c2e07ada1dab0073496
calgrimes <calgrimes29@gmail.com>
2022-03-29 18:55:24 +0100

    code relocation

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ $entityBody = file_get_contents('php://input');
 
 $file = 'readings.txt';
 
+file_put_contents($file, $entityBody . PHP_EOL, FILE_APPEND | LOCK_EX);
+
 if(isset($_GET["clear"])) {
     file_put_contents($file, "");
     $_GET["clear"] = NULL
 }
 
-file_put_contents($file, $entityBody . PHP_EOL, FILE_APPEND | LOCK_EX);
 
 ?>
