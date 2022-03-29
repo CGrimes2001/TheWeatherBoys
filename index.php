eb7c4057e2358f279f74fc33ef4d6b47141aa3b9
calgrimes <calgrimes29@gmail.com>
2022-03-29 12:25:13 +0100

    get JSON post requests

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// if (!empty($_GET['readings'])) {
+//     $fp = fopen('readings.txt', 'a');
+//     fwrite($fp, $_GET['readings'] . PHP_EOL);   
+//     fclose($fp);  
+// }
+
+$entityBody = file_get_contents('php://input');
+
+$file = 'readings.txt';
+
+
+file_put_contents($file, $entityBody, FILE_APPEND | LOCK_EX);
